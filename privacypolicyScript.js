a2da2cb949b8d6d8c1eaf883e5accb3e01ccfc73
WizCodGamez <wizcodgamez@gmail.com>
2023-01-26 01:32:47 +0530

    updated webpages

--- privacypolicyScript.js
+++ privacypolicyScript.js
@@ -1,6 +1,6 @@
 let calcScrollValue = () => {
-    let scrollProgress = document.getElementById("progress");
-    let progressValue = document.getElementById("progress-value");
+    let scrollProgress = document.getElementById("progresss");
+    let progressValue = document.getElementById("progresss-value");
     let pos = document.documentElement.scrollTop;
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrollValue = Math.round((pos * 100) / calcHeight);
